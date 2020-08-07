01b306ccc430a4e25298ae54830640bf62dba51f
Ian Davidson <ian.davidson@onepeloton.com>
2020-08-07 13:18:25 -0400

    add state / optimistic update / format

--- lib/picker_web/live/page_live.ex
+++ lib/picker_web/live/page_live.ex
@@ -5,26 +5,43 @@ defmodule PickerWeb.PageLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, names: [], name: "", selectedName: "")}
+    {:ok, %HTTPoison.Response{status_code: 200, body: body}} =
+      HTTPoison.get(
+        "https://textdb.dev/api/data/054be034-502c-4d18-a9d9-479ea9f60c22",
+        [{"accepts", "application/json"}],
+        []
+      )
+
+    {:ok,
+     assign(socket,
+       names: body |> Poison.decode!() |> Map.get("_json"),
+       name: "",
+       selectedName: ""
+     )}
   end
 
   @impl true
   def handle_event("add-name", %{"name" => new_name}, socket) do
-    json_names =
-      socket.assigns.names
-      |> Poison.encode!
+    new_name_list = socket.assigns.names ++ [new_name]
 
-    {:ok, _res} = HTTPoison.post "https://textdb.dev/data/054be034-502c-4d18-a9d9-479ea9f60c22",
+    json_names =
+      new_name_list
+      |> Poison.encode!()
+
+    Task.start(fn ->
+      HTTPoison.post(
+        "https://textdb.dev/api/data/054be034-502c-4d18-a9d9-479ea9f60c22",
         json_names,
         [{"Content-Type", "application/json"}]
+      )
+    end)
 
-    Logger.info(new_name)
-    Logger.info(inspect socket.assigns.names)
-    {:noreply, assign(
-        socket,
-        names: socket.assigns.names ++ [new_name],
-        name: ""
-      )}
+    {:noreply,
+     assign(
+       socket,
+       names: new_name_list,
+       name: ""
+     )}
   end
 
   def handle_event("select-name", _val, socket) do
@@ -32,17 +49,18 @@ defmodule PickerWeb.PageLive do
       socket.assigns.names
       |> Enum.random()
 
-    {:noreply, assign(
-        socket,
-        selectedName: name
-      )}
+    {:noreply,
+     assign(
+       socket,
+       selectedName: name
+     )}
   end
 
   def handle_event("select-special", _val, socket) do
-    {:noreply, assign(
-        socket,
-        selectedName: "ETHANN"
-      )}
+    {:noreply,
+     assign(
+       socket,
+       selectedName: "ETHANN"
+     )}
   end
-
 end
